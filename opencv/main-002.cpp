d81206168aa67ba4e75580464de67fe323ba2232
huanghl <18344730514@163.com>
2020-10-09 16:28:32 +0800

    修改

--- opencv/main-002.cpp
+++ opencv/main-002.cpp
@@ -14,22 +14,7 @@ int main(int argc, char** argv) {
 	namedWindow("input image", CV_WINDOW_AUTOSIZE);
 	imshow("input image", src);
 	
-	/*
-	int cols = (src.cols-1) * src.channels();
-	int offsetx = src.channels();
-	int rows = src.rows;
 
-	dst = Mat::zeros(src.size(), src.type());
-	for (int row = 1; row < (rows - 1); row++) {
-		const uchar* previous = src.ptr<uchar>(row - 1);
-		const uchar* current = src.ptr<uchar>(row);
-		const uchar* next = src.ptr<uchar>(row + 1);
-		uchar* output = dst.ptr<uchar>(row);
-		for (int col = offsetx; col < cols; col++) {
-			output[col] = saturate_cast<uchar>(5 * current[col] - (current[col- offsetx] + current[col+ offsetx] + previous[col] + next[col]));
-		}
-	}
-	*/
 	double t = getTickCount();
 	Mat kernel = (Mat_<char>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
 	filter2D(src, dst, src.depth(), kernel);
